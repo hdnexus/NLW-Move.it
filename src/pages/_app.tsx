2041a0186bb184d1b5f61ccd37bb4922e90d0d9d
Gabriel <dotaes22@gmail.com>
2021-02-26 10:22:23 -0300

    Finalizado dia 5

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,9 @@
 import "../styles/global.css";
 import { ChallengesProvider } from "../contexts/ChallengesContext";
 import React from "react";
-import { CountdownProvider } from "../contexts/CountdownContext";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <ChallengesProvider>
-      <Component {...pageProps} />]
-    </ChallengesProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
